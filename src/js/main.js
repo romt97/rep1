5d70e02983807155604e312ad8c6e4cad09c8032
romt97 <tkacnenko.roman97@yandex.by>
2022-11-11 03:15:34 +0300

    Create footer slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-//here will by JavaScript code ...
+$('.rev_slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        }
+    ]
+});
