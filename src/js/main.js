9fac10367b335b113318a001fce159b690eb0944
romt97 <tkacnenko.roman97@yandex.by>
2022-11-14 00:33:49 +0300

    Edit code

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
-$(".phonenumber").mask("+7(999)999-99-99");
+// $(".phonenumber").mask("+7(999)999-99-99");
 
-function buttonClick(elem) {
-	elem.value = 'Заявка отправлена!';
-}
+// function buttonClick(elem) {
+// 	elem.value = 'Заявка отправлена!';
+// };
+
+$('slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-nav'
+	arrows: true
+});
+$('.slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	dots: true,
+	centerMode: true,
+	focusOnSelect: true
+});
